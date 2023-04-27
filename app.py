2a2ba1bebaaa77ec2632af3a233e4d93027dd0d7
0xphoton <42502920+maxtalwar@users.noreply.github.com>
2023-04-27 16:58:46 -0700

    added basic login functionality, integrated most of the code with flask

--- app.py
+++ app.py
@@ -6,11 +6,78 @@ app = Flask(__name__)
 
 app.secret_key = "LInPyX4PpZx6hAHofg=="
 
-# TODO: Fill in methods and routes
+@app.route('/', methods=["GET", "POST"])
+def home():
+    return redirect(url_for("login"))
 
-@app.before_first_request
-def setup():
-    init_db()
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+
+        email_exists = db_session.query(User.email).filter(User.email == email).first() is not None
+        username_exists = db_session.query(User.username).filter(User.username == username).first() is not None
+
+        if password != confirm_password:
+            flash("Passwords do not match", "error")
+            return render_template("signup.html")
+        elif email_exists:
+            flash("email already taken", "error")
+            return render_template("signup.html")
+        elif username_exists:
+            flash("username already taken", "error")
+            return render_template("signup.html")
+        else:
+            user = User(username=username, email=email, password=password)
+            db_session.add(user)
+            db_session.commit()
+
+            session["username"] = username
+
+            return redirect(url_for("login"))
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        correct_user_password = db_session.query(User.password).filter(User.username == username).first()[0]
+        print("debug")
+
+        if password == correct_user_password:
+            session["username"] = username
+
+            return redirect(url_for("profile"))
+        else:
+            flash("incorrect username or password", "error")
+            print(password)
+            print(correct_user_password)
+            return render_template("login.html")
+        
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
+        
+@app.route("/logout")
+def logout():
+    if "username" in session:
+        session.pop("username")
+        flash("You've been logged out", "info")
+    return redirect(url_for("login"))
+    
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -8,24 +8,25 @@ from sqlalchemy import ForeignKey, Column, INTEGER, TEXT
 from sqlalchemy.orm import relationship
 from database import Base
 
-class User:
+class User(Base):
     __tablename__ = "users"
 
     # columns
-    user_id = Column("id", INTEGER, primary_key=True)
+    id = Column("id", INTEGER, primary_key=True)
     username = Column("username", TEXT, nullable=False)
     email = Column("email", TEXT, nullable=False)
     password = Column("password", TEXT, nullable=False)
-    first_name = Column("first_name", TEXT, nullable=False)
-    last_name = Column("last_name", TEXT, nullable=False)
+    
+    # stretch goals
+    first_name = Column("first_name", TEXT, nullable=True)
+    last_name = Column("last_name", TEXT, nullable=True)
     profile_pic = Column("profile_pic", TEXT, nullable=False)
 
     # relationships
     reviews = relationship("Review", back_populates="user")
 
     # constructor
-    def __init__(self, user_id, username, email, password, first_name, last_name, profile_pic):
-        self.user_id = user_id
+    def __init__(self, username, email, password, first_name="", last_name="", profile_pic="static/assets/profile_photo.png"):
         self.username = username
         self.email = email
         self.password = password
@@ -33,13 +34,13 @@ class User:
         self.last_name = last_name
         self.profile_pic = profile_pic
 
-class Review:
+class Review(Base):
     __tablename__ = "reviews"
 
     # columns
-    review_id = Column("id", INTEGER, primary_key=True)
-    user_id = Column("user_id", ForeignKey, nullable=False)
-    race_id = Column("race_id", ForeignKey, nullable=False)
+    id = Column("id", INTEGER, primary_key=True)
+    user_id = Column("user_id", ForeignKey("users.id"), nullable=False)
+    race_id = Column("race_id", ForeignKey("races.id"), nullable=False)
     rating = Column("rating", INTEGER, nullable=False)
     review_date = Column("review_date", TEXT, nullable=False)
     review_text = Column("review_text", TEXT, nullable=True)
@@ -49,18 +50,17 @@ class Review:
     race = relationship("Race", back_populates="reviews")
 
     # constructor
-    def __init__(self, review_id, user_id, rating, review_date, review_text):
-        self.review_id = review_id
+    def __init__(self, user_id, rating, review_date, review_text):
         self.user_id = user_id
         self.rating = rating
         self.review_date = review_date
         self.review_text = review_text
 
-class Race:
+class Race(Base):
     __tablename__ = "races"
 
     # columns
-    race_id = Column("race_id", INTEGER, primary_key=True)
+    id = Column("id", INTEGER, primary_key=True)
     race_name = Column("race_name", TEXT, nullable=False)
     race_location = Column("race_location", TEXT, nullable=False)
     race_date = Column("race_date", TEXT, nullable=False)
@@ -72,8 +72,7 @@ class Race:
     reviews = relationship("Review", back_populates="race")
 
     # constructor
-    def __init__(self, race_id, race_name, race_location, race_date, race_distance, race_website, race_description):
-        self.race_id = race_id
+    def __init__(self, race_name, race_location, race_date, race_distance, race_website, race_description):
         self.race_name = race_name
         self.race_location = race_location
         self.race_date = race_date
